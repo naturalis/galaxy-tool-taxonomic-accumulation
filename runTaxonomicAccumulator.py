ad74e4c35617dadb4bf793b15133c3e2b7ff6d84
dickgroenenberg <dickgroenenberg@gmail.com>
2020-04-21 12:18:27 +0200

    typo correction

--- runTaxonomicAccumulator.py
+++ runTaxonomicAccumulator.py
@@ -67,7 +67,7 @@ def getAccumulationZip(dirTemporary, flOutput, lstTaxonRanks):
     for strFiles in os.listdir(dirTemporary):
         if strFiles.endswith(".tabular"):
             tblReadInput = pd.read_table(dirTemporary + strFiles)
-            for strRow in tblReadInput["Taxonomy"]:
+            for strRow in tblReadInput["#Taxonomy"]:
                 strTaxonLine = strRow.split("/")
                 strTaxonLine = [strName.strip(" ") for strName in strTaxonLine]
                 for intTaxonPosition in range(7):
@@ -223,7 +223,7 @@ def getBlastOutput(flOutput, lstTaxonDics, lstTaxonRanks):
 def getAccumulationBlast(flInput, flOutput, lstTaxonRanks):
     tblReadInput = pd.read_table(flInput)
     lstTaxonDics = [{} for intTaxonCount in range(7)]
-    for strRow in tblReadInput["Taxonomy"]:
+    for strRow in tblReadInput["#Taxonomy"]:
         strTaxonLine = strRow.split("/")
         strTaxonLine = [strName.strip(" ") for strName in strTaxonLine]
         for intTaxonPosition in range(7):
